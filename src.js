f4f198f6a19f8a364fcec5ee3ffdab4b7d1bc590
atorov <atoro.atorov@gmail.com>
2016-02-10 14:02:10 +0200

    V0.2.60210.3: Issue#2.2: Use jQuery(function($) {... instead of .ready(function() {... ; Issue#2.3: Use 'not-a-string' object label names; Issue#2.4: Support 4 indentation spaces.

--- src.js
+++ src.js
@@ -1,135 +1,136 @@
 "use strict"
-$(document).ready(function() {
-  var taskID = {
-    val  : 1000,
-    inc  : function() {
-       return ++this.val;
+jQuery(function($) {
+    // $(document).ready(function() {
+    var taskID = {
+        val  : 1000,
+        inc  : function() {
+            return ++this.val;
+        }
     }
-  }
-  var taskList = [];
+    var taskList = [];
 
-  // Delete task (when is done)
-  function delTask(btn, list, id) {
-    // Find task index
-    var index = $.map(list, function(e, i) {
-      if(e.id === parseInt(btn.id)){ return i; }
+    // Delete task (when is done)
+    function delTask(btn, list, id) {
+        // Find task index
+        var index = $.map(list, function(e, i) {
+            if(e.id === parseInt(btn.id)){ return i; }
+        });
+        // Update task list (remove element from array)
+        list.splice(index, 1);
+        // Update task list div (remove task's sub-div)
+        $(btn).parent().remove();
+        // Update status pannel
+        showStat(list, id);
+    }
+
+    // Edit task list item
+    function editTaskListItem(btn, list, id) {
+        // Find task index
+        var index = $.map(list, function(e, i) {
+            if(e.id === parseInt(btn.id)){ return i; }
+        });
+        var task = list[index];
+        // Update task list div (edit mode)
+        var span_id = $("<span/>").
+        append("ID_" + task.id + ": ");
+        var in_name = $("<input/>").
+        attr({
+            type      : "text",
+            value     : task.name,
+            id        : "" + task.id + "_inEditName",
+            size      : 32,
+            maxlength : 32,
+            autofocus : "",
+            onclick   : "this.select()"
+        });
+        var btn_update = $("<button/>").
+        append("Update").
+        click(function() {
+            // Update task list array
+            list[index].name = $("#" + task.id + "_inEditName").val();
+            // Update task list div
+            $(this).parent().remove();
+            $("#divTaskList").prepend(genTaskItem(list[index], list, id));
+        });
+        $("#" + btn.id).parent().
+        empty().
+        append(span_id, in_name, btn_update);
+    }
+
+    // Generate task list item
+    function genTaskItem(task, list, id) {
+        var span_id = $("<span/>").append("ID_" + task.id + ": ");
+        var span_name = $("<span/>").append("" + task.name + " ");
+        var btn_edit = $("<button/>").
+        attr("id", "" + task.id + "_btnTaskItemEdit").
+        append("Edit").
+        click(function() { editTaskListItem(this, list, id); });
+        var btn_done = $("<button/>").
+        attr("id", "" + task.id + "_btnTaskItemDone").
+        append("Done!").
+        click(function() { delTask(this, list, id); });
+        return $("<div/>").attr({
+            id      : "" + task.id + "_divTaskItem",
+            style   : "background-color:#bdc3c7",
+        }).append(span_id, span_name, btn_edit, btn_done);
+    }
+
+    // Show stat
+    function showStat(list, id) {
+        var span_last_id = $("<span/>").
+        append("Last ID: " + id.val + "; ");
+        var span_task_num = $("<span/>").
+        append("Tasks: " + list.length + "; ");
+        $("#divStatus").empty().
+        append(span_last_id, span_task_num);
+    }
+
+    // Task prototype
+    function Task(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Main functionality implementation starts here...
+    // *** New task pannel
+    // Show/Hide pannel
+    $("#btnAddNewToggle").click(function(){
+        $("#divAddNew").toggle();
     });
-    // Update task list (remove element from array)
-    list.splice(index, 1);
-    // Update task list div (remove task's sub-div)
-    $(btn).parent().remove();
-    // Update status pannel
-    showStat(list, id);
-  }
 
-  // Edit task list item
-  function editTaskListItem(btn, list, id) {
-    // Find task index
-    var index = $.map(list, function(e, i) {
-      if(e.id === parseInt(btn.id)){ return i; }
+    //Add new task (Closures!)
+    $("#btnAddNew").click(function() {
+        return function(id, list) {
+            var new_id = id.inc();
+            var new_name = $("#inAddNewName").val();
+            var new_task = new Task(new_id, new_name);
+            // Update task list array
+            list.push(new_task);
+            // Update task list div
+            $("#divTaskList").prepend(genTaskItem(new_task, list, id));
+            // Update status pannel
+            showStat(list, id);
+        }(taskID, taskList);
     });
-    var task = list[index];
-    // Update task list div (edit mode)
-    var span_id = $("<span/>").
-      append("ID_" + task.id + ": ");
-    var in_name = $("<input/>").
-      attr({
-        "type"      : "text",
-        "value"     : task.name,
-        "id"        : "" + task.id + "_inEditName",
-        "size"      : 32,
-        "maxlength" : 32,
-        "autofocus" : "",
-        "onclick"   : "this.select()"
-      });
-    var btn_update = $("<button/>").
-      append("Update").
-      click(function() {
-        // Update task list array
-        list[index].name = $("#" + task.id + "_inEditName").val();
-        // Update task list div
-        $(this).parent().remove();
-        $("#divTaskList").prepend(genTaskItem(list[index], list, id));
-      });
-    $("#" + btn.id).parent().
-      empty().
-      append(span_id, in_name, btn_update);
-  }
 
-  // Generate task list item
-  function genTaskItem(task, list, id) {
-    var span_id = $("<span/>").append("ID_" + task.id + ": ");
-    var span_name = $("<span/>").append("" + task.name + " ");
-    var btn_edit = $("<button/>").
-      attr("id", "" + task.id + "_btnTaskItemEdit").
-      append("Edit").
-      click(function() { editTaskListItem(this, list, id); });
-    var btn_done = $("<button/>").
-      attr("id", "" + task.id + "_btnTaskItemDone").
-      append("Done!").
-      click(function() { delTask(this, list, id); });
-    return $("<div/>").attr({
-      "id"      : "" + task.id + "_divTaskItem",
-      "style"   : "background-color:#bdc3c7",
-    }).append(span_id, span_name, btn_edit, btn_done);
-  }
+    // *** Status pannel
+    // Show/Hide pannel
+    $("#btnStatusToggle").click(function(){
+        $("#divStatus").toggle();
+    });
 
-  // Show stat
-  function showStat(list, id) {
-    var span_last_id = $("<span/>").
-      append("Last ID: " + id.val + "; ");
-    var span_task_num = $("<span/>").
-      append("Tasks: " + list.length + "; ");
-    $("#divStatus").empty().
-      append(span_last_id, span_task_num);
-  }
+    // *** Task list pannel
+    // Show/Hide pannel
+    $("#btnTaskListToggle").click(function(){
+        $("#divTaskList").toggle();
+    });
 
-  // Task prototype
-  function Task(id, name) {
-    this.id = id;
-    this.name = name;
-  }
+    // *** About pannel
+    // Show/Hide pannel
+    $("#btnAboutToggle").click(function(){
+        $("#divAbout").toggle();
+    });
 
-  // Main functionality implementation starts here...
-  // *** New task pannel
-  // Show/Hide pannel
-  $("#btnAddNewToggle").click(function(){
-    $("#divAddNew").toggle();
-  });
-
-  //Add new task (Closures!)
-  $("#btnAddNew").click(function() {
-    return function(id, list) {
-      var new_id = id.inc();
-      var new_name = $("#inAddNewName").val();
-      var new_task = new Task(new_id, new_name);
-      // Update task list array
-      list.push(new_task);
-      // Update task list div
-      $("#divTaskList").prepend(genTaskItem(new_task, list, id));
-      // Update status pannel
-      showStat(list, id);
-    }(taskID, taskList);
-  });
-
-  // *** Status pannel
-  // Show/Hide pannel
-  $("#btnStatusToggle").click(function(){
-    $("#divStatus").toggle();
-  });
-
-  // *** Task list pannel
-  // Show/Hide pannel
-  $("#btnTaskListToggle").click(function(){
-    $("#divTaskList").toggle();
-  });
-
-  // *** About pannel
-  // Show/Hide pannel
-  $("#btnAboutToggle").click(function(){
-    $("#divAbout").toggle();
-  });
-
-  // *** Update status pannel
-  showStat(taskList, taskID);
+    // *** Update status pannel
+    showStat(taskList, taskID);
 });
